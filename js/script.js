fee776bda0365d239e5854d61ad716f6a9cc0946
Crysta Jeffcoat <crystajeffcoat@gmail.com>
2020-05-08 18:46:02 -0400

    added psuedo code and variables for password criteria

--- js/script.js
+++ js/script.js
@@ -12,3 +12,23 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+//password criteria 
+function generatePassword() {
+    //prompt for password length
+    function passwordLength(passLength) {
+        var passLength = prompt("How long would you like your password to be? \nLength must be at least 8 characters and no more than 128");
+        if (prompt.value < 8 && prompt.value > 128) {
+            alert("Password must be at least 8 characters long and no more than 128");
+            passwordLength();
+        }else {
+            passLength = prompt.value
+        }
+    }
+
+    //prompt for character types
+    function charTypes() {
+
+    }
+}
+generatePassword(passwordLength);
